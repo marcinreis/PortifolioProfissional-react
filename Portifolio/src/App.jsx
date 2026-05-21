1ccd8d66cec136c44e8872bee5b55f2a61ea1e27
marcio reis <reismarcio143@gmail.com>
2026-05-21 13:46:18 -0300

    dasda

--- Portifolio/src/App.jsx
+++ Portifolio/src/App.jsx
@@ -1,36 +1,16 @@
-// Importando os estilos do Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// Importando os componentes que criamos
-import Navbar from "./componentes/Navbar";
-import Hero from "./componentes/Hero";
-import Projetos from "./componentes/Projetos";
-import Contato from "./componentes/Contato";
+import Navbar from "./componentes/navbar";
+import Hero from "./componentes/hero";
+import Projetos from "./componentes/projetos";
+import Contato from "./componentes/contato";
 
 function App() {
   return (
-    // Container principal da aplicação
-    <div
-      style={{
-        backgroundColor: "#0a0a0a",
-        minHeight: "100vh",
-        // Importa a fonte Space Mono do Google Fonts
-        fontFamily: "'Space Mono', monospace",
-      }}
-    >
-      {/* Importação da fonte via link no head (coloque isso no index.html também) */}
-      {/* <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap" rel="stylesheet" /> */}
-
-      {/* Navbar fixo no topo */}
+    <div style={{ backgroundColor: "#0a0a0a", minHeight: "100vh" }}>
       <Navbar />
-
-      {/* Seção de apresentação */}
       <Hero />
-
-      {/* Seção de projetos */}
       <Projetos />
-
-      {/* Seção de contato */}
       <Contato />
     </div>
   );
